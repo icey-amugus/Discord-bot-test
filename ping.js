1672a4a45100019286a6aa11ff7404d4ec707497
cheems <85101415+onlycheems@users.noreply.github.com>
2022-05-06 15:05:51 +0200

    Add files via upload

---
+++ ping.js
@@ -0,0 +1,8 @@
+module.export = {
+    name: 'server',
+    description: 'There si support server',
+    execute(message, args){
+        message.channel.send('https://discord.gg/xzhc54Mn2H')
+    }
+
+}
